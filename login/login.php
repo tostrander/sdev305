b3c915a5b33404431ca87a15adcccbde8883c31e
Tina Ostrander <tina.j.ostrander@gmail.com>
2020-11-25 10:55:55 -0800

    Add files via upload

--- login/login.php
+++ login/login.php
@@ -37,6 +37,9 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
         $_SESSION['loggedin'] = true;
 
         //Redirect to page the user was on
+        if (!isset($_SESSION['page'])) {
+            $_SESSION['page'] = 'index.php';
+        }
         header("location: " . $_SESSION['page']);
 
     }
