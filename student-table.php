7af6b7cad363fabcde38905441fe5ab44c21daf3
Tina Ostrander <tina.j.ostrander@gmail.com>
2021-02-08 10:46:41 -0800

    Display student data in an HTML table
    Move database connection to a separate file and display student data in an HTML table

---
+++ student-table.php
@@ -0,0 +1,66 @@
+<?php
+/* student.php
+   Reads from a database
+*/
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Connect to database - BAD SECURITY
+require ('connect.php');
+$cnxn = mysqli_connect($hostname, $username, $password, $database);
+echo "Connected!";
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>GRC Students</title>
+</head>
+<body>
+<h1>GRC Students</h1>
+<table>
+    <thead>
+        <th>SID</th><th>First</th><th>Last</th>
+    </thead>
+
+
+<?php
+    //1. Define the query
+    $sql = "SELECT * FROM student";
+
+    //2. Send the query to the db
+    $result = mysqli_query($cnxn, $sql);
+
+    //3. Print the result
+    //var_dump($result);
+    foreach ($result as $row) {
+        //var_dump($row);
+
+        $sid = $row['sid'];
+        $last = $row['last'];
+        $first = $row['first'];
+
+        echo "<tr><td>$sid</td><td>$first</td><td>$last</td></tr>";
+    }
+?>
+</table>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
