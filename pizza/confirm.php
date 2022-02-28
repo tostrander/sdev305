a2e976d50b62402a5a45acafbe328824ee4b6bb0
Tina Ostrander <tina.j.ostrander@gmail.com>
2022-02-28 10:57:40 -0800

    Add files via upload

--- pizza/confirm.php
+++ pizza/confirm.php
@@ -1,5 +1,19 @@
 <?php
-    include("includes/header.html");
+    //Turn on error reporting
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+
+    //If the form has not been submitted, redirect user to home page form
+    //Must come BEFORE any HTML output
+    if (empty($_POST)) {
+        header('location: index.php');
+    }
+
+    //Connect to database, creates $cnxn
+    //Require terminates script if file not found
+    require('/home2/tostrand/connect.php');
+    include('includes/header.html');
+    require('includes/functions.php');
 ?>
 
 <div id="main" class="container">
@@ -13,11 +27,8 @@
 
     <?php
 
-        //Turn on error reporting
-        ini_set('display_errors', 1);
-        error_reporting(E_ALL);
-
     /*
+        // After header()
         echo "<pre>";
         // $_POST is an "autoglobal" array
         var_dump($_POST);
@@ -25,13 +36,100 @@
     */
 
         //Move form data into variables
-        $fname = $_POST['fname'];
-        $lname = $_POST['lname'];
-        $email = $_POST['email'];
-        $method = $_POST['method'];
-        $size = $_POST['size'];
+
+        //Initialize variables
+        $fname = "";
+        $lname = "";
+        $email = "";
+        $method = "";
+        $size = "";
+        $comment = "";
+        $toppings = "";
+
+        //Validate the data
+        $isValid = true;
+
+        //first name
+        if (!validName($_POST['fname'])) {
+            echo "<p>First name is required</p>";
+            $isValid = false;
+        }
+        else {
+            $fname = $_POST['fname'];
+        }
+
+        //last name
+        if (!validName($_POST['lname'])) {
+            echo "<p>Last name is required</p>";
+            $isValid = false;
+        }
+        else {
+            $lname = $_POST['lname'];
+        }
+
+        //email is optional, but if provided must be valid
+        if (!validEmail($_POST['email'])) {
+            echo "<p>Email address is not valid</p>";
+            $isValid = false;
+        }
+        else {
+            $email = $_POST['email'];
+        }
+
+        //If no method is selected
+        if (!isset($_POST['method'])) {
+            echo "<p>Method must be selected</p>";
+            $isValid = false;
+        }
+        //Method is selected, but contains a bad value
+        else if (!validMethod($_POST['method'])) {
+            echo "<p>You tried to spoof me!</p>";
+            $isValid = false;
+        }
+        //Method is good
+        else {
+            $method = $_POST['method'];
+        }
+
+
+        //If no size is selected
+        if (!isset($_POST['size'])) {
+            echo "<p>Size must be selected</p>";
+            $isValid = false;
+        }
+        //Size is selected, but contains a bad value
+        else if (!validSize($_POST['size'])) {
+            echo "<p>Select a valid size</p>";
+            $isValid = false;
+        }
+        //Size is good
+        else {
+            $size = $_POST['size'];
+        }
+
+        //Toppings have been selected
+        if (isset($_POST['toppings'])) {
+
+            //Check for spoofing
+            if (!validToppings($_POST['toppings'])) {
+                echo "<p>You tried to spoof me!</p>";
+                $isValid = false;
+            }
+            //Data is valid
+            else {
+                $toppings = implode(", ", $_POST['toppings']);
+            }
+        }
+
+        //If the data is not valid, stop processing
+        if (!$isValid) {
+            return;
+        }
+
+        //empty is used for text boxes and text areas
+        //isset is used for checkboxes, radio buttons, and drop downs
         $comment = $_POST['comment'];
-        $toppings = implode(", ", $_POST['toppings']);
+
 
         //Define constants
         define("TAX_RATE", 0.065);
@@ -63,9 +161,23 @@
         //Format price
         $price = number_format($price, 2);
 
+
+        //Write to database
+        $sql = "INSERT INTO pizza (`fname`, `lname`, `address`, `size`, `toppings`, `method`, `price`, `comment`) 
+            VALUES ('$fname', '$lname', '$email', '$size', '$toppings', '$method', '$price', '$comment')";
+        echo "<p>$sql</p>";
+        $success = mysqli_query($cnxn, $sql);
+
+        if (!$success) {
+            echo "<p>We're sorry... something went wrong. Please call us...</p>";
+            return;
+        }
+
+
         //Display an Order Summary
         //echo "<h1>Thank you for your order, $fname!</h1>";
         //echo '<h1>Thank you for your order, '.$fname.'!</h1>';
+
 
         thanks($fname);
         thanks();
@@ -122,7 +234,7 @@
         );
 
             INSERT INTO pizza (`fname`, `lname`, `address`, `size`, `toppings`, `method`, `price`, `comment`)
-            VALUES ('Gavin', 'Sherman', NULL, 'small', 'pepperoni, sausage', 'pickup', 9.95, NULL);
+            VALUES ('Duc', 'Tram', '123 Oak Lane', 'XL', 'pepperoni, sausage', 'delivery', '25.00', '');
 
          */
 
--- pizza/includes/functions.php
+++ pizza/includes/functions.php
@@ -13,3 +13,68 @@ function getSizes()
         'med'=>'Medium', 'large'=>'Large', 'xl'=>"Extra Large");
     return $sizes;
 }
+
+function validName($name)
+{
+    return !empty($name);
+}
+
+function validEmail($email)
+{
+    /*
+    if (!empty($email) && filter_var($email, FILTER_VALIDATE_EMAIL)){
+        return true;
+    }
+    else {
+        return false;
+    }
+    */
+
+    return !empty($email) && filter_var($email, FILTER_VALIDATE_EMAIL);
+}
+
+function validMethod($method)
+{
+    /*
+    if ($method == "pickup" || $method == "delivery")
+        return true;
+    else
+        return false;
+    */
+
+    return $method == "pickup" || $method == "delivery";
+}
+
+function validSize($size)
+{
+    /*
+    $validSizes = getSizes();
+    if (array_key_exists($size, $validSizes) && $size != "none")
+        return true;
+    else
+        return false;
+    */
+
+    return array_key_exists($size, getSizes()) && $size != "none";
+}
+
+function validToppings($userToppings)
+{
+    $availableToppings = getToppings();
+
+    foreach($userToppings as $topping) {
+        if (!in_array($topping, $availableToppings)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+echo "I am in the functions file";
+$test = "large";
+if (validSize($test)) {
+    echo "<p>$test is Good</p>";
+}
+else {
+    echo "<p>$test is Bad</p>";
+}
